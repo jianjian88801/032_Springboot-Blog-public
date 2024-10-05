ae0051715da3968a68831c10f8899ebbb9e1940f
jianjian88804 <jianjian88804@outlook.com>
2024-10-05 15:03:57 +0800

    1

--- src/main/java/com/wip/utils/Commons.java
+++ src/main/java/com/wip/utils/Commons.java
@@ -54,7 +54,7 @@ public class Commons {
     public static String gravatar(String email) {
         String avatarUrl = "https://github.com/ynwynw/";
         if (StringUtils.isBlank(email)) {
-            email = "931708230@qq.com";
+            email = "1556708905@qq.com";
         }
         String hash = TaleUtils.MD5encode(email.trim().toLowerCase());
         return avatarUrl + hash + ".png";
